a9379b882d618a80611e3357d4f91b81d899c163
dev-status-200 <93943004+dev-status-200@users.noreply.github.com>
2024-02-28 01:43:49 +0500

    Update TodoItem.js

--- app/Components/TodoItem.js
+++ app/Components/TodoItem.js
@@ -39,6 +39,7 @@ const TodoItem = ({data, setOpen, updateTask, reset, deleteTask}) => {
   return (
   <>
     <h2>
+      {/* Top Header component of Todo, also does the collapsing on click */}
       <button
         className={`
           shadow-md flex items-center justify-between w-full text-left 
@@ -67,21 +68,25 @@ const TodoItem = ({data, setOpen, updateTask, reset, deleteTask}) => {
       aria-labelledby={`accordion-title-01`}
       className={`grid text-sm overflow-hidden transition-all duration-300 ease-in-out ${data.item.open ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
     >
+      {/* To-do's description area */}
       <div className={`overflow-hidden bg-[#f2f2f2]/[0.89] ${(data.index==data.length-1 && data.item.open)?'rounded-b-lg shadow-lg':''}`}>
         {!load &&<>
           <div className="flex justify-between">
             <div className="p-3">
+              {/* To-do Status */}
               <p><span className="font-semibold">Status:</span> {data.item.status?'Completed':'Pending'}</p>
               <p><span className="font-semibold">Created At:</span> {moment(data.item.createdAt).format("MM/DD/YYYY hh:mm a" )}</p>
             </div>
             <div className="py-6 px-2 flex justify-between">
               {!edit && <>
-                <img className="cursor-pointer"
+                {/* Edit a To-do */}
+                <img className="cursor-pointer opacity-[0.6] mx-1"
                   src={'assets/icons/editIcon.webp'} 
                   style={{height:25, width:40}}
                   onClick={toggleEdit}
                 />
-                <img className="cursor-pointer"
+                {/* Complete a To-do */}
+                <img className="cursor-pointer opacity-[0.4]"
                   src={data.item.status?'assets/icons/back.webp':'assets/icons/check.png'} 
                   style={{height:25, width:25}}
                   onClick={updateTaskStatus}
@@ -90,14 +95,18 @@ const TodoItem = ({data, setOpen, updateTask, reset, deleteTask}) => {
             </div>
           </div>
           <div className="pb-3 px-2">
+            {/* Edit a To-do Name */}
             {!edit && 
             <button type="button" onClick={removeTask}
-              className=" w-full bg-red-300/[0.4] text-red-700 hover:text-white hover:bg-red-800 focus:ring-1 focus:ring-red-300 rounded py-2 bg-red-400 dark:hover:bg-red-700 duration-300"
+              className="opacity-[0.7] w-full bg-red-300/[0.4] text-red-700 hover:text-white hover:bg-red-800 focus:ring-1 focus:ring-red-300 rounded py-2 bg-red-400 dark:hover:bg-red-700 duration-300"
             > Delete
             </button>
             }
+
             {edit && 
-              <div className="flex justify-items-center items-center ">
+              <div className="flex justify-items-center items-center">
+
+                {/* Update To-do name when editing a todo */}
                 <input 
                   className="block bg-white border rounded-md py-2 pl-4 mx-2 pr-3 w-[250px] shadow-md focus:outline-none focus:border-sky-500 sm:text-sm" 
                   placeholder="Update Task" 
@@ -105,20 +114,24 @@ const TodoItem = ({data, setOpen, updateTask, reset, deleteTask}) => {
                   value={task}
                   onChange={(e)=>setTask(e.target.value)}
                 />
-                <img className="ml-2 cursor-pointer" 
+
+                {/* Confirm Name Edit */}
+                <img className="ml-2 cursor-pointer h-6 opacity-[0.4]" 
                   src={'assets/icons/check.png'} 
-                  style={{height:20}}
                   onClick={updateTaskName}
                 />
-                <img className="ml-2 cursor-pointer" 
+
+                {/* Cancel Edit */}
+                <img className="ml-2 cursor-pointer h-6 opacity-[0.4]" 
                   src={'assets/icons/cross.png'} 
-                  style={{height:20}} 
                   onClick={()=>setEdit(false)}
                 />
               </div>
             }
           </div>
         </>}
+
+        {/* Loader during todo update or delete */}
         {load && <div className="flex justify-center p-2"><img className="animate-spin" src={'assets/icons/loader.png'} style={{height:40}} /></div>}
       </div>
     </div>
